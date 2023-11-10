926cb038478ea1f7a696e0aa1c2edb02e2f882fc
zeshalondrag <isip_a.v.samarskiy@mpt.ru>
2023-11-10 19:46:22 +0300

    Explorer.exe

---
+++ PW7/ArrowMenu.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+public static class ArrowMenu
+{
+    public static int ShowMenu(string[] items)
+    {
+        int currentChoice = 0;
+
+        while (true)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (i == currentChoice)
+                {
+                    Console.Write("-> ");
+                }
+                else
+                {
+                    Console.Write("   ");
+                }
+
+                Console.WriteLine(items[i]);
+            }
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            Console.Clear();
+
+            if (keyInfo.Key == ConsoleKey.UpArrow)
+            {
+                currentChoice = (currentChoice - 1 + items.Length) % items.Length;
+            }
+            else if (keyInfo.Key == ConsoleKey.DownArrow)
+            {
+                currentChoice = (currentChoice + 1) % items.Length;
+            }
+            else if (keyInfo.Key == ConsoleKey.Enter)
+            {
+                return currentChoice;
+            }
+            else if (keyInfo.Key == ConsoleKey.Escape)
+            {
+                return -1;
+            }
+        }
+    }
+}
---
+++ PW7/Explorer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+public static class Explorer
+{
+    private static void Main()
+    {
+        DriveInfo[] allDrives = DriveInfo.GetDrives();
+        string[] options = new string[allDrives.Length];
+
+        for (int i = 0; i < allDrives.Length; i++)
+        {
+            options[i] = $"{allDrives[i].Name} " +
+                         $"{allDrives[i].AvailableFreeSpace / (1024 * 1024 * 1024):N2} ГБ свободно из " +
+                         $"{allDrives[i].TotalSize / (1024 * 1024 * 1024):N2} ГБ";
+        }
+
+        int selectedDrive = ArrowMenu.ShowMenu(options);
+        string selectedDriveName = allDrives[selectedDrive].Name;
+
+        folders(selectedDriveName);
+    }
+
+    private static void folders(string path)
+    {
+        DirectoryInfo dirInfo = new DirectoryInfo(path);
+
+        DirectoryInfo[] directories = dirInfo.GetDirectories();
+        FileInfo[] files = dirInfo.GetFiles();
+
+        string[] options = new string[directories.Length + files.Length];
+
+        int i = 0;
+        foreach (DirectoryInfo directory in directories)
+        {
+            options[i++] = String.Format("{0,-45} {1,25}", directory.Name, directory.CreationTime.ToString("dd.MM.yyyy HH:mm"));
+        }
+
+        foreach (FileInfo file in files)
+        {
+            options[i++] = String.Format("{0,-45} {1,25} {2,10}", file.Name, file.CreationTime.ToString("dd.MM.yyyy HH:mm"), file.Extension);
+        }
+
+        int selectedOption = ArrowMenu.ShowMenu(options);
+
+        if (selectedOption == -1)
+        {
+            if (dirInfo.Parent != null)
+            {
+                folders(dirInfo.Parent.FullName);
+            }
+            else
+            {
+                Main();
+            }
+        }
+        else if (selectedOption < directories.Length)
+        {
+            folders(directories[selectedOption].FullName);
+        }
+        else
+        {
+            string selectedFilePath = files[selectedOption - directories.Length].FullName;
+            openFiles(selectedFilePath);
+        }
+    }
+    private static void openFiles(string filePath)
+    {
+        try
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = filePath,
+                UseShellExecute = true
+            };
+
+            Process.Start(startInfo);
+            
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Что-то вы не то открыли: {ex.Message}");
+        }
+    }
+}
